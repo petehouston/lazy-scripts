85d75c5e6677180475bba7218d0a85d4042c602a
Pete Houston <contact@petehouston.com>
2018-07-05 14:03:16 +0700

    Add Python SSH upload script

---
+++ py-ssh-upload/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import paramiko
+import time
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+try:
+    fd = open('config.txt', 'r')
+except IOError:
+    print('[ERROR] Fail to open config file')
+    quit()
+
+server, username, password = fd.readline().strip().split(':')
+localfile, remotefile = fd.readline().strip().split(':')
+
+print('localfile: ', localfile)
+print('remotefile: ', remotefile)
+
+
+prevtime = time.time()
+currtime = time.time()
+
+try:
+    ssh.connect(server, username=username, password=password)    
+    # stdin, stdout, stderr = ssh.exec_command('rm -rf /home/nhn/config.txt')
+    # for lin in stdout.readlines():
+        # print(lin.strip())
+except paramiko.AuthenticationException:
+    print('[ERROR] Authentication failure')
+    quit()
+except:
+    print('[ERROR] Unknown error')
+    quit()
+
+
+
+def reportUploadProgress(bTrans, bTotal):
+    global currtime
+    global prevtime
+    currtime = time.time()
+    if currtime - prevtime < 1:
+        return
+    prevtime = currtime
+    percent = int(bTrans / bTotal * 100)
+    if bTrans < bTotal:
+        print("[UPLOAD] Progress: %d%%" % (percent))
+    else:
+        print('[UPLOAD] Completed!')
+
+sftp = ssh.open_sftp()
+sftp.put(localfile, remotefile, callback=reportUploadProgress)
+ssh.close()
