4d02bd3b7918c0d356b847eb31ffa570988097e9
Pete Houston <contact@petehouston.com>
2018-09-25 00:36:53 +0700

    Refactor rendering.

--- workplace-latest-noti/index.js
+++ workplace-latest-noti/index.js
@@ -4,8 +4,25 @@ const CONFIG = require('./config.json');
 // const WORKPLACE_URL = 'https://' + CONFIG.workplace_id + '.facebook.com/work/landing/input';
 const WORKPLACE_URL = 'https://' + CONFIG.workplace_id + '.facebook.com/login/?next=%2F&email=' + encodeURIComponent(CONFIG.email);
 
+function renderHeader() {
+    console.log('========================================================');
+    console.log('| WORKPLACE AUTOMATION TOOLS                           |');
+    console.log('| Developed by Pete Houston <contact@petehouston.com>  |');
+    console.log('========================================================');
+    console.log('Your Workplace: https://' + CONFIG.workplace_id + '.facebook.com/');
+    console.log('Your email    : ' + CONFIG.email);
+    console.log('---------- Please wait for a moment --------------------\n');
+}
+
+function renderFooter() {
+    console.log('--------------------------------------------------------');
+    console.log('Feel free to contact if you have any question!');
+}
+
 (async (cb) => {
-    const browser = await puppeteer.launch({headless: false})
+    renderHeader();
+
+    const browser = await puppeteer.launch({headless: true})
     const page = await browser.newPage()
     
     await page.goto(WORKPLACE_URL);
@@ -43,27 +60,18 @@ const WORKPLACE_URL = 'https://' + CONFIG.workplace_id + '.facebook.com/login/?n
         cb({
             unread_count,
             items: list
-        })
+        });
     }
 
-})(render);
+    renderFooter();
 
-function render(data) {
-    console.log('========================================================');
-    console.log('| WORKPLACE AUTOMATION TOOLS                           |');
-    console.log('| Developed by Pete Houston <contact@petehouston.com>  |');
-    console.log('========================================================');
-    console.log('Your Workplace: https://' + CONFIG.workplace_id + '.facebook.com/');
-    console.log('Your email    : ' + CONFIG.email);
-    console.log('---------- Please wait for a moment --------------------\n');
+})(renderResult);
+
+function renderResult(data) {
     console.log('[UNREAD NOTIFICATION] ' + data.unread_count);
     
-
     data.items.forEach(i => {
         console.log('   - "' + i.text + '"' + ' [about ' + i.time + ' ago]');        
         console.log('     Visit: ' + i.url);
     });
-
-    console.log('--------------------------------------------------------');
-    console.log('Feel free to contact if you have any question!');
 }
